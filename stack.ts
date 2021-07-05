b2d7ab5bb1d83989d88c3876563e3aa9ee56a261
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-07-05 22:09:00 +0900

    add generics

--- stack.ts
+++ stack.ts
@@ -1,32 +1,32 @@
-interface Stack {
+interface Stack<T> {
   readonly size: number;
-  push(value: string): void;
-  pop(): string;
+  push(value: T): void;
+  pop(): T;
 }
 
-type StackNode = {
-  readonly value: string;
-  readonly next?: StackNode;
+type StackNode<T> = {
+  readonly value: T;
+  readonly next?: StackNode<T>;
 };
 
-class StackImpl implements Stack {
+class StackImpl<T> implements Stack<T> {
   private _size: number = 0;
-  private head?: StackNode;
+  private head?: StackNode<T>;
 
   constructor(private capacity: number) {}
 
   get size() {
     return this._size;
   }
-  push(value: string): void {
+  push(value: T): void {
     if (this.size === this.capacity) {
       throw new Error("Stack is full!");
     }
-    const node: StackNode = { value, next: this.head };
+    const node: StackNode<T> = { value, next: this.head };
     this.head = node;
     this._size++;
   }
-  pop(): string {
+  pop(): T {
     if (this.head == null) {
       throw new Error("Stack is empty!");
     }
@@ -37,7 +37,7 @@ class StackImpl implements Stack {
   }
 }
 const stack = new StackImpl(4);
-stack.push("11");
+stack.push(11);
 stack.push("Bob 2");
 stack.push("ys 3");
 while (stack.size !== 0) {
